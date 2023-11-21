f50ff8528a7cca8e5adc0b2b8818e03365ea5d95
Pavel <pavelcistjakov@gmail.com>
2023-11-21 08:49:06 +0000

    splitting state into internal implementation and public api

--- app/src/main/java/com/example/recipesapp/RecipesViewModel.kt
+++ app/src/main/java/com/example/recipesapp/RecipesViewModel.kt
@@ -1,30 +1,25 @@
 package com.example.recipesapp
 
+import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import retrofit2.HttpException
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.Retrofit
-import java.net.ConnectException
-import java.net.UnknownHostException
 
 class RecipesViewModel: ViewModel() {
     private val repository = RecipesRepository()
-    val state = mutableStateOf(
+    private val _state = mutableStateOf(
         RecipeScreenState(
             recipes = listOf(),
             isLoading = true
         )
     )
+    val state: State<RecipeScreenState>
+        get() = _state
     private val errorHandler = CoroutineExceptionHandler{ _, exception ->
         exception.printStackTrace()
-        state.value = state.value.copy(
+        _state.value = _state.value.copy(
             error = exception.message,
             isLoading = false
         )
@@ -37,7 +32,7 @@ class RecipesViewModel: ViewModel() {
     private fun getRecipes() {
         viewModelScope.launch(errorHandler) {
             val recipes = repository.getAllRecipes()
-            state.value = state.value.copy(
+            _state.value = _state.value.copy(
                 recipes = recipes,
                 isLoading = false
             )
@@ -46,7 +41,7 @@ class RecipesViewModel: ViewModel() {
     fun toggleFavorite(id: Int, oldValue: Boolean) {
         viewModelScope.launch {
             val updatedRecipes = repository.toggleFavoriteRecipe(id, oldValue)
-            state.value = state.value.copy(
+            _state.value = _state.value.copy(
                 recipes = updatedRecipes
             )
         }
